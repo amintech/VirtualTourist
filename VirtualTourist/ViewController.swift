92241003ce3afdea682155859bf606f0a10d4812
Amin Saleh <ameen.0100@gmail.com>
2019-01-17 16:56:47 +0300

    Initial Commit

---
+++ VirtualTourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VirtualTourist
+//
+//  Created by  AminSaleh on 11/05/1440 AH.
+//  Copyright © 1440 AminTech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
