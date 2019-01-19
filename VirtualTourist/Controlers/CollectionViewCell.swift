a339e64ebbc8947fcad108b844e647d47cf9c7a5
Amin Saleh <ameen.0100@gmail.com>
2019-01-19 13:47:25 +0300

    Adding CollectionViewCell

---
+++ VirtualTourist/Controlers/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  VirtualTourist
+//
+//  Created by  AminSaleh on 13/05/1440 AH.
+//  Copyright © 1440 AminTech. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var image: UIImageView!
+    
+}
