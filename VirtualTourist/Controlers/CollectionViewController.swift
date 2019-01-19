9bc58313b95fa230a687382090f05271f1df72ef
Amin Saleh <ameen.0100@gmail.com>
2019-01-19 13:44:54 +0300

    Adding ViewControllers

---
+++ VirtualTourist/Controlers/CollectionViewController.swift
@@ -0,0 +1,21 @@
+//
+//  CollectionViewController.swift
+//  VirtualTourist
+//
+//  Created by  AminSaleh on 13/05/1440 AH.
+//  Copyright © 1440 AminTech. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ VirtualTourist/Controlers/MapViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MapViewController.swift
+//  VirtualTourist
+//
+//  Created by  AminSaleh on 13/05/1440 AH.
+//  Copyright © 1440 AminTech. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController, MKMapViewDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
